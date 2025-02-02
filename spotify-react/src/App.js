13b047f9476d0c296dd94292ac7f43fc1e8297b2
Domingos Santiago <dmgssantiago@gmail.com>
2025-02-02 20:19:56 -0300

    Atualização de arquivos com React
    Reacriação do site usando o React

---
+++ spotify-react/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './componentes/Header/Header'
+import Main from './componentes/Main/Main'
+import Sidebar from './componentes/Sidebar/Sidebar'
+import Footer from './componentes/Footer/Footer'
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Main />
+    <Sidebar />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
